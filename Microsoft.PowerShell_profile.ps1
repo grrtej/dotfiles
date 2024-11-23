d82eec06487a489b5763d62eaf1eaec8d300d8f6
Gurtej Singh <grrtej@gmail.com>
2024-11-23 05:22:02 -0800

    update pwsh profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,8 +2,9 @@ set-psreadlineoption -editmode emacs
 set-psreadlinekeyhandler -key tab -function menucomplete
 set-psreadlineoption -predictionviewstyle listview
 
-function ll { ls -force @args }
-function rmf { rm -force @args }
-function vim { & "C:\Program Files\Vim\vim91\vim.exe" @args }
-function gvim { & "C:\Program Files\Vim\vim91\gvim.exe" @args }
-new-alias n gvim
+new-alias vim  "C:\Program Files\Vim\vim91\vim.exe"
+new-alias gvim "C:\Program Files\Vim\vim91\gvim.exe"
+new-alias n    gvim
+new-alias vlc  "C:\Program Files\VideoLAN\VLC\vlc.exe"
+function  ll   { ls -force @args }
+function  rmf  { rm -force @args }
