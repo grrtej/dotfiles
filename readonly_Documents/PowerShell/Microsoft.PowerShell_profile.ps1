93ffac56f5475ccdecd09810a27a09170b900b84
Gurtej Saini <grrtej@gmail.com>
2026-06-26 22:18:17 -0500

    add pwsh profile

---
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+set-psreadlineoption -editmode emacs
+set-psreadlinekeyhandler -chord tab -function menucomplete
+
+function ll { ls -force @args }
+function rmf { rm -force @args }
+
+new-alias vim "C:\Program Files\Vim\current\gvim.exe"
